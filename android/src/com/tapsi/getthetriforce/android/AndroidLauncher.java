470f5760050e991f1daac4e6bba2dee1da661c32
RBlischk <rebecca.bl@gmx.de>
2016-01-23 18:59:49 +0100

    ...

---
+++ android/src/com/tapsi/getthetriforce/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.tapsi.getthetriforce.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.tapsi.getthetriforce.GetTheTriforce;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GetTheTriforce(), config);
+	}
+}
---
+++ html/src/com/tapsi/getthetriforce/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.tapsi.getthetriforce.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.tapsi.getthetriforce.GetTheTriforce;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new GetTheTriforce();
+        }
+}
